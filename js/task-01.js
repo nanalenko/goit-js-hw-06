ad5017eabd4674cace73c446579c3339ef2b6f72
Tanya Kyrylenko <nanalenko1905@gmail.com>
2022-10-11 21:36:14 +0300

    добавила html и скрипты

---
+++ js/task-01.js
@@ -0,0 +1,17 @@
+const list =document.querySelector('#categories');
+const listItems = list.querySelectorAll('li.item');
+ console.log(`Number of categories :${listItems.length}`);
+
+const amount = () => {
+  listItems.forEach(item=>{
+    const firstEl = item.firstElementChild;
+    const lastEl = item.lastElementChild;
+
+  console.log("Category :",firstEl.textContent);
+  console.log("Elements:",lastEl.children.length);
+    }   
+ ) 
+}
+amount()
+
+
